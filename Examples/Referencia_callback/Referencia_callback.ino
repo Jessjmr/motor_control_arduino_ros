d0976063af96fb00ef79497d90183d38969abf71
Jess <j.maldonador@outlook.com>
2022-11-21 21:20:13 -0600

    Integracion control velocidad y ROS

--- Examples/Referencia_callback/Referencia_callback.ino
+++ Examples/Referencia_callback/Referencia_callback.ino
@@ -4,34 +4,22 @@
 #define USE_USBCON
 #include <ros.h>
 #include <std_msgs/Int32.h>
-#include <std_msgs/Float32.h>
 //Libreria Encoder
 #include <Encoder.h>
 //Libreria Motor
 #include <Adafruit_MotorShield.h>
 
 float t1=0,u,t=0,int_e=0,kp,ki,ref,int_bound,ref_ros;
-float a,ref_smooth=0,Hz;
+float a,ref_f=0,Hz;
 long g_ant,grados;
 unsigned long timeold = 0;
-
-float a_n;
-char buffer[250];
 
 //Nodo de ROS nh
 ros::NodeHandle  nh;
 
-//Publisher ROS-referencia
+//Publisher ROS
 std_msgs::Int32 info_sub1;
 ros::Publisher vref("vref", &info_sub1);
-
-//Publisher ROS-rpm
-std_msgs::Float32 info_rpm;
-ros::Publisher velocity("velocity", &info_rpm);
-
-//Publisher ROS-ref smooth
-std_msgs::Int32 info_refsmooth;
-ros::Publisher refsmooth("refsmooth", &info_refsmooth);
 
 //Callback Subcriber and publisher
 void valor_ref( const std_msgs::Int32& v_ref){
@@ -78,15 +66,9 @@ void setup() {
   //Set-up ROS and subscriber ROS
   nh.initNode();
   nh.advertise(vref);
-  nh.advertise(velocity);
-  nh.advertise(refsmooth);
   nh.subscribe(sub1);
   
-  if (!nh.getParam("~an",&a_n,3)){
-  //default values
-    a_n=100;
-  }
-
+  
   //Variables de control
   kp=2;
   ki=10; 
@@ -94,7 +76,7 @@ void setup() {
   // saturación de la integral 
   int_bound=1.8;
 
-  //Referencia, configurable por ROS
+  //Referencia, configurable por serial
   ref=0;
   a = 600;
 
@@ -102,54 +84,12 @@ void setup() {
   Hz=20;  
 }
 
-float ref_suave (float ref)
-{
-//Función de referencia suave
-  
-  if (abs(ref-ref_smooth)<abs(a/Hz)){
-    ref_smooth=ref;        
-  }
-  else if(ref_smooth>ref){
-    if(abs(ref-ref_smooth)>90){
-      ref_smooth = ref_smooth - 0.3*(a/Hz);      
-    }
-    else{         
-      ref_smooth = ref_smooth - (a/Hz);
-    }    
-  } else if (ref_smooth<ref) {
-    if(abs(ref-ref_smooth)>90){
-      ref_smooth = ref_smooth + 0.3*(a/Hz);      
-    }
-    else{         
-      ref_smooth = ref_smooth + (a/Hz);
-    }
-  }  
-  return ref_smooth;
-}
-
-int sat (float x,float cota)
-{
-//Función de saturación
-  
-  if (x>cota){
-      x=cota;
-  }
-  else if (x<-cota){
-      x=-cota;
-  }
-  return x;
-}
-
 void loop() {
 
 if (millis() - timeold >= (1000/Hz)) {
-  
-  float t2; // Variable de tiempo en el instante actual
-  float d_finitas; //Calculo de velocidad usando diferencias finitas en grados/segundo
-  float rpm; // Variable Revoluciones por Minuto RPM=(Grados/segundo)/6
-  float dt; //Variable diferencial de tiempo (instante actual)-(instante anterior)
-  float e; // Variable del error Velocidad de referencia - velocidad actual
-  long grados_actuales; // Variable de posición del encoder actual
+
+  float t2,d_finitas,rpm,dt,e;
+  long grados_actuales;
 
   // Lectura actual de posición
   grados_actuales = 0.162162162*myEnc.read();
@@ -163,23 +103,53 @@ if (millis() - timeold >= (1000/Hz)) {
   timeold = millis();
   //Control P  
   //u=(255/120)*(k*ref+(1-k)*rpm);
+  if (abs(ref-ref_f)<abs(a/Hz)){
+    ref_f=ref;        
+  }
+  else if(ref_f>ref){
+    if(abs(ref-ref_f)>90){
+      ref_f = ref_f - 0.3*(a/Hz);      
+    }
+    else{         
+      ref_f = ref_f - (a/Hz);
+    }    
+  } else if (ref_f<ref) {
+    if(abs(ref-ref_f)>90){
+      ref_f = ref_f + 0.3*(a/Hz);      
+    }
+    else{         
+      ref_f = ref_f + (a/Hz);
+    }
+  }
   
-  ref_smooth=ref_suave(ref);
   
-  e=ref_smooth-rpm; //Error
+  e=ref_f-rpm; //Error
   if (abs(e)<10){
     int_e=int_e+e*dt; // Integral del error
   }
   
   // Saturación del integrador
-  int_e= sat(int_e,int_bound); 
-  
+  if (int_e>int_bound){
+    int_e=int_bound;
+  }
+  else if (int_e<-int_bound){
+    int_e=-int_bound;
+  }
   //Control PI
   u=(255/120)*(rpm+(kp*e)+(ki*int_e));
   //u=255;
-  // Saturación del controlador
-  u=sat(u,255);
-    
+  //Serial.print(u*(120.0/255.0));
+  //Serial.print(",");
+  //Serial.print(ref_f);
+  //Serial.print(",");
+  //Serial.println(rpm);
+  //Saturación del control
+  if (u>255){
+      u=255;
+  }
+  else if (u<-255){
+      u=-255;
+  }
   if (u<0){
   myMotor->run(BACKWARD);}
   else  {
@@ -188,12 +158,9 @@ if (millis() - timeold >= (1000/Hz)) {
   myMotor->setSpeed(abs(u));
 
   //ROS Publisher
-  info_rpm.data=rpm;
-  velocity.publish(&info_rpm);
-  info_refsmooth.data=ref_smooth;
-  refsmooth.publish(&info_refsmooth);
   nh.spinOnce();
   
+
   t1=t2;
   g_ant=grados_actuales; 
 }  
